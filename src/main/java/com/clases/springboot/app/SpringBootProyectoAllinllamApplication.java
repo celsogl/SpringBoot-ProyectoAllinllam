e81fbf29763115b75ab83579a51b975e30605df5
celsogl <celsogonzalesleon@gmail.com>
2023-11-13 23:37:12 -0500

    init

---
+++ src/main/java/com/clases/springboot/app/SpringBootProyectoAllinllamApplication.java
@@ -0,0 +1,13 @@
+package com.clases.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProyectoAllinllamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProyectoAllinllamApplication.class, args);
+	}
+
+}
